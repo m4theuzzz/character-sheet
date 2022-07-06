32a5bc3ef8c0781c5fd1526bcf9669a83423130b
m4theuzzz <mat.vsantos@outlook.com>
2022-07-06 03:13:10 -0300

    try deploy

--- modules/Database.js
+++ modules/Database.js
@@ -7,10 +7,10 @@ const fs = require('fs');
 class Database {
     constructor(databaseDirectory, isUser = false) {
         if (isUser) {
-            this.pathToDatabases = path.join(path.join(path.join(path.join(__dirname, '../'), '../'), databaseDirectory), '');
+            this.pathToDatabases = path.join(path.join(path.join(__dirname, '../'), databaseDirectory), '');
             this.requiredTables = ['users'];
         } else {
-            this.pathToDatabases = path.join(path.join(path.join(path.join(path.join(__dirname, '../'), '../'), databaseDirectory), process.env.USER_ID), '');
+            this.pathToDatabases = path.join(path.join(path.join(path.join(__dirname, '../'), databaseDirectory), process.env.USER_ID), '');
             this.requiredTables = ['characters', 'items', 'spells'];
         }
 
